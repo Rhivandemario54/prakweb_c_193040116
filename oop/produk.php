295107e2943884ca10494bcdd1ef2ebfc770ac2a
Rhivan Gabrille Demarrio <rhivandemario54@gmail.com>
2022-10-03 12:06:48 +0700

    Property dan Method

--- oop/produk.php
+++ oop/produk.php
@@ -1,13 +1,39 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game
-class produk {
+class Produk {
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = "0";
+           
+           public function getLabel(){
+            return"$this->penulis, $this->penerbit";
+           }
+        }
+        
 
-}
+// $produk1 = new produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk1 = new produk();
-$produk2 = new Produk();
+// $produk2 = new produk();
+// $produk2->judul = "uncharted";
+// $produk2->tambahPrpperty = "hahaha";
+// var_dump($produk2);
 
+$produk3 = new Produk();
+$produk3->judul = "naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "shonen jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sonny Computer";
+$produk4->harga = 250000;
+
+echo "Komik : ". $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
 ?>
