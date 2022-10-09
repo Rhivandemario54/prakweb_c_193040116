3bde1a30ca63dd000eb35f086230e98007ac0122
Rhivan Gabrille Demarrio <rhivandemario54@gmail.com>
2022-10-09 14:48:39 +0700

    mvc database wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASEURL' , 'http://localhost/prakweb_c_193040116/mvc/public');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
+?>
--- mvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL' , 'http://localhost/prakweb_c_193040116/mvc/public');
